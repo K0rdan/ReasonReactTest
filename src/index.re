b01a419b0177730d37c18a5ef4d9e713bd0bc480
K0rdan <christau_benjamin@hotmail.com>
2018-07-08 12:02:58 +0200

    Initial commit

---
+++ src/index.re
@@ -0,0 +1,4 @@
+ReactDOMRe.renderToElementWithId(<div>
+  <Component1 message="Hello!" />
+  <Component2 greeting="Hello!" />
+</div>, "root");
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const outputDir = path.join(__dirname, "build/");
+
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  entry: './src/index.bs.js',
+  mode: isProd ? 'production' : 'development',
+  output: {
+    path: outputDir,
+    publicPath: outputDir,
+    filename: 'index.js',
+  },
+  devServer: {
+    contentBase: './build'
+  },    
+};
